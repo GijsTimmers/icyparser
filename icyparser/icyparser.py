7562584f766cda71ae221466e321d34143e0f9b0
GijsTimmers <timmers.gijs@gmail.com>
2016-07-27 17:36:17 +0200

    first succesful StreamTitle parsing

--- icyparser/icyparser.py
+++ icyparser/icyparser.py
@@ -21,4 +21,26 @@ clean_dict = dict(rsp_headers)
 for k in sorted(clean_dict.keys()):
     print(k + ": " + clean_dict[k])
 
+#chunksize = 1024
+chunksize = 16000
+
+with open("bestand.mp3", "wb") as bestand:
+    count = 0
+    lengte_metadata = 0
+    metadata = ""
+    while True:
+        stream = rsp.read(1)
+        count += 1
+
+        if count == 16001:
+            lengte_metadata = int(ord(stream))*16
+            print(lengte_metadata)
+
+        if lengte_metadata != 0:
+            if count >= 16002 and count <= 16001 + lengte_metadata:
+                #print(stream)
+                print(stream.decode("utf-8", "ignore"), end="")
+
+        if count >= 16500:
+            break
 
